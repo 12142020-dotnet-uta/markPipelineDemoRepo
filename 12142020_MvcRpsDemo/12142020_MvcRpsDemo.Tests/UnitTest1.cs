e2d9569ad06f593efd5563b78ab504ff91842ed2
Mark Moore <mark.moore@revature.com>
2021-01-15 14:24:04 -0600

    adds to project to test .yml file.

---
+++ 12142020_MvcRpsDemo/12142020_MvcRpsDemo.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using System;
+using Xunit;
+using ModelLayer;
+using BusinessLogicLayer;
+
+namespace _12142020_MvcRpsDemo.Tests
+{
+	public class UnitTest1
+	{
+		[Fact]
+		public void SaveGameReturnsOne()
+		{
+			BusinessLogicClass blc = new BusinessLogicClass();
+			int hundred = blc.SaveGame(10);
+			Assert.Equal(1, hundred);
+		}
+	}
+}
---
+++ 12142020_MvcRpsDemo/RepositoryLayer/DbContextClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using ModelLayer;
+
+namespace RepositoryLayer
+{
+	public class DbContextClass : DbContext
+	{
+		public DbSet<Player> players { get; set; }
+		public DbSet<Round> rounds { get; set; }
+		public DbSet<Match> matches { get; set; }
+
+		public DbContextClass() { }
+		public DbContextClass(DbContextOptions options) : base(options) { }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder options)
+		{
+			if (!options.IsConfigured)
+			{
+				options.UseSqlServer("Server=tcp:myshopazuredbserver.database.windows.net,1433;Initial Catalog=MyShopAzure;Persist Security Info=False;User ID=markcmoore;Password=cU5tod33qUal;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+			}
+		}
+	}
+}
