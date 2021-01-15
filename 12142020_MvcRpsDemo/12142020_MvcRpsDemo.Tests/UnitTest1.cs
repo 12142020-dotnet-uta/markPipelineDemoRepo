af2b22eea6494677fc54e814ada42576c5964df7
Mark Moore <mark.moore@revature.com>
2021-01-15 15:53:34 -0600

    adds to project to test .yml file.

--- 12142020_MvcRpsDemo/12142020_MvcRpsDemo.Tests/UnitTest1.cs
+++ 12142020_MvcRpsDemo/12142020_MvcRpsDemo.Tests/UnitTest1.cs
@@ -5,14 +5,14 @@ using BusinessLogicLayer;
 
 namespace _12142020_MvcRpsDemo.Tests
 {
-    public class UnitTest1
-    {
-        [Fact]
-        public void SaveGameReturnsOne()
-        {
-            BusinessLogicClass blc = new BusinessLogicClass();
-            int hundred = blc.SaveGame(10);
-            Assert.Equal(10, hundred);
-        }
-    }
+	public class UnitTest1
+	{
+		[Fact]
+		public void SaveGameReturnsOne()
+		{
+			BusinessLogicClass blc = new BusinessLogicClass();
+			int hundred = blc.SaveGame(10);
+			Assert.Equal(1, hundred);
+		}
+	}
 }
